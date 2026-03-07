7f123f03697de2e015a2fbeac3825d01954a63fe
Sam <samicandelaria@gmail.com>
2026-03-07 11:35:52 -0600

    Ejercicio 07 de Marzo [Flatlist]. Samantha Candelaria Mora.

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,52 @@
+import { Tabs } from "expo-router";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export default function Layout() {
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: '#44BBA4' }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    tabBarIcon: () => (
+                        <FontAwesome name="home" size={24} color="black" />
+                    )
+                }}
+            />
+            <Tabs.Screen 
+                name="buscar"
+                options={{
+                    tabBarIcon: () => (
+                        <FontAwesome name="search" size={24} color="black" />
+                    )
+                }}
+            />
+            <Tabs.Screen 
+                name="config" 
+                options={{
+                    tabBarIcon: () => (
+                        <Feather name="settings" size={24} color="black" />
+                    )
+                }}
+            />
+            <Tabs.Screen 
+                name="perfil" 
+                options={{
+                    tabBarIcon: () => (
+                        <AntDesign name="user" size={24} color={"black"} />
+                    )
+                }} 
+            />
+            <Tabs.Screen 
+                name="alumnos" 
+                options={{
+                    tabBarIcon: () => (
+                        <MaterialIcons name="class" size={24} color="black" />
+                    )
+                }} 
+            />
+        </Tabs>
+    );
+}
---
+++ app/(tabs)/perfil.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Perfil() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.texto}>Perfil</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    texto: { fontSize: 28, fontWeight: 'bold' }
+});
